eb5f0a68fc88a22e7b6a1fc6301b43a8bf3db9cb
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-12-23 21:55:30 +0400

    Utility-app-final-version

---
+++ vending-machine-final.py
@@ -0,0 +1,372 @@
+print("""██╗██╗░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░
+╚█║╚█║░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗
+░╚╝░╚╝░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║
+░░░░░░░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║
+░░░░░░░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝
+░░░░░░░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░
+
+░██████╗███╗░░██╗███████╗░█████╗░██╗░░██╗██╗░░░██╗░██████╗███╗░░██╗░█████╗░░█████╗░██╗░░██╗░██████╗
+██╔════╝████╗░██║██╔════╝██╔══██╗██║░██╔╝╚██╗░██╔╝██╔════╝████╗░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝
+╚█████╗░██╔██╗██║█████╗░░███████║█████═╝░░╚████╔╝░╚█████╗░██╔██╗██║███████║██║░░╚═╝█████═╝░╚█████╗░
+░╚═══██╗██║╚████║██╔══╝░░██╔══██║██╔═██╗░░░╚██╔╝░░░╚═══██╗██║╚████║██╔══██║██║░░██╗██╔═██╗░░╚═══██╗
+██████╔╝██║░╚███║███████╗██║░░██║██║░╚██╗░░░██║░░░██████╔╝██║░╚███║██║░░██║╚█████╔╝██║░╚██╗██████╔╝
+╚═════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░╚═╝░░╚══╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+░█████╗░░█████╗░░░░  ██╗░░░██╗███████╗███╗░░██╗██████╗░██╗███╗░░██╗░██████╗░
+██╔══██╗██╔══██╗░░░  ██║░░░██║██╔════╝████╗░██║██╔══██╗██║████╗░██║██╔════╝░
+██║░░╚═╝██║░░██║░░░  ╚██╗░██╔╝█████╗░░██╔██╗██║██║░░██║██║██╔██╗██║██║░░██╗░
+██║░░██╗██║░░██║░░░  ░╚████╔╝░██╔══╝░░██║╚████║██║░░██║██║██║╚████║██║░░╚██╗
+╚█████╔╝╚█████╔╝██╗  ░░╚██╔╝░░███████╗██║░╚███║██████╔╝██║██║░╚███║╚██████╔╝
+░╚════╝░░╚════╝░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░
+
+███╗░░░███╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗███████╗██╗██╗██╗
+████╗░████║██╔══██╗██╔══██╗██║░░██║██║████╗░██║██╔════╝██║╚█║╚█║
+██╔████╔██║███████║██║░░╚═╝███████║██║██╔██╗██║█████╗░░██║░╚╝░╚╝
+██║╚██╔╝██║██╔══██║██║░░██╗██╔══██║██║██║╚████║██╔══╝░░╚═╝░░░░░░
+██║░╚═╝░██║██║░░██║╚█████╔╝██║░░██║██║██║░╚███║███████╗██╗░░░░░░
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░░░░░""")
+print("""██╗████████╗  ██╗░██████╗  ░█████╗░███╗░░██╗  ██╗░░██╗░█████╗░███╗░░██╗░█████╗░██████╗░
+██║╚══██╔══╝  ██║██╔════╝  ██╔══██╗████╗░██║  ██║░░██║██╔══██╗████╗░██║██╔══██╗██╔══██╗
+██║░░░██║░░░  ██║╚█████╗░  ███████║██╔██╗██║  ███████║██║░░██║██╔██╗██║██║░░██║██████╔╝
+██║░░░██║░░░  ██║░╚═══██╗  ██╔══██║██║╚████║  ██╔══██║██║░░██║██║╚████║██║░░██║██╔══██╗
+██║░░░██║░░░  ██║██████╔╝  ██║░░██║██║░╚███║  ██║░░██║╚█████╔╝██║░╚███║╚█████╔╝██║░░██║
+╚═╝░░░╚═╝░░░  ╚═╝╚═════╝░  ╚═╝░░╚═╝╚═╝░░╚══╝  ╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝
+
+████████╗░█████╗░  ██████╗░██████╗░░█████╗░██╗░░░██╗██╗██████╗░███████╗
+╚══██╔══╝██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗██║░░░██║██║██╔══██╗██╔════╝
+░░░██║░░░██║░░██║  ██████╔╝██████╔╝██║░░██║╚██╗░██╔╝██║██║░░██║█████╗░░
+░░░██║░░░██║░░██║  ██╔═══╝░██╔══██╗██║░░██║░╚████╔╝░██║██║░░██║██╔══╝░░
+░░░██║░░░╚█████╔╝  ██║░░░░░██║░░██║╚█████╔╝░░╚██╔╝░░██║██████╔╝███████╗
+░░░╚═╝░░░░╚════╝░  ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝╚═════╝░╚══════╝
+
+░██████╗███████╗██████╗░██╗░░░██╗██╗░█████╗░███████╗  ███████╗░█████╗░██████╗░  ░█████╗░██╗░░░██╗██████╗░
+██╔════╝██╔════╝██╔══██╗██║░░░██║██║██╔══██╗██╔════╝  ██╔════╝██╔══██╗██╔══██╗  ██╔══██╗██║░░░██║██╔══██╗
+╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝██║██║░░╚═╝█████╗░░  █████╗░░██║░░██║██████╔╝  ██║░░██║██║░░░██║██████╔╝
+░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██║██║░░██╗██╔══╝░░  ██╔══╝░░██║░░██║██╔══██╗  ██║░░██║██║░░░██║██╔══██╗
+██████╔╝███████╗██║░░██║░░╚██╔╝░░██║╚█████╔╝███████╗  ██║░░░░░╚█████╔╝██║░░██║  ╚█████╔╝╚██████╔╝██║░░██║
+╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚══════╝  ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝  ░╚════╝░░╚═════╝░╚═╝░░╚═╝
+
+░█████╗░██╗░░░██╗░██████╗████████╗░█████╗░███╗░░░███╗███████╗██████╗░░██████╗░░░
+██╔══██╗██║░░░██║██╔════╝╚══██╔══╝██╔══██╗████╗░████║██╔════╝██╔══██╗██╔════╝░░░
+██║░░╚═╝██║░░░██║╚█████╗░░░░██║░░░██║░░██║██╔████╔██║█████╗░░██████╔╝╚█████╗░░░░
+██║░░██╗██║░░░██║░╚═══██╗░░░██║░░░██║░░██║██║╚██╔╝██║██╔══╝░░██╔══██╗░╚═══██╗░░░
+╚█████╔╝╚██████╔╝██████╔╝░░░██║░░░╚█████╔╝██║░╚═╝░██║███████╗██║░░██║██████╔╝██╗""")
+print("""Fᴏʀ Iᴛᴇᴍs 20-29, ᴘʟᴇᴀsᴇ ᴀsᴋ ᴛʜᴇ ᴇᴍᴘʟᴏʏᴇᴇ ɪɴsɪᴅᴇ ᴛʜᴇ sᴛᴏʀᴇ ᴛᴏ ᴡᴀʀᴍ ᴜᴘ ᴛʜᴇ ᴍᴇᴀʟ ᴏғ ʏᴏᴜʀ ᴄʜᴏɪᴄᴇ.""")
+#In terms of stocks, I have decided to categorize each Snack, Drink & Meal in order to have a more
+# show a  variety of products for the customers to choose from.
+#The snacks will hold candies, chips, crackers and easily consumable food
+#for the customer to enjoy.
+Sneaky_snacks =  [  
+   {
+      "itemId": 0,
+      "itemName": "Maltesers candy pack",
+      'itemPrice': 2.00,
+      'itemStock': 14
+#As is shown above, each item will hold specific ID and Price corresponding to the Product name and it stock.
+#This will helpthe code in identifying each item as its own individual product to avoid choosing any unwanted product at the time.
+   },{
+      "itemId": 1,
+      "itemName": "SNICKERS bar",
+      'itemPrice': 3.00,
+      'itemStock': 15
+   },{
+      "itemId": 2,
+      "itemName": "Kit-Kat Bar 150g",
+      'itemPrice': 5.00,
+      'itemStock': 6
+   },{
+      "itemId": 3,
+      "itemName": "Haribo Goldbears fruit juice gummies 160g",
+      'itemPrice': 10.00,
+      'itemStock': 7
+   },{
+      "itemId": 4,
+      "itemName": "Skittles candy pack 14x38g",
+      'itemPrice': 2.00,
+      'itemStock': 8
+   },{
+      "itemId": 5,
+      "itemName": "MARS bar",
+      'itemPrice': 3.00,
+      'itemStock': 9
+   },{
+      "itemId": 6,
+      "itemName": "M&M's candy pack",
+      'itemPrice': 3.00,
+      'itemStock': 10
+   },{
+      "itemId": 7,
+      "itemName": "BOUNTY chocolate bar ",
+      'itemPrice': 2.00,
+      'itemStock': 11
+   },{
+      "itemId": 8,
+      "itemName": "",
+      'itemPrice': 2.00,
+      'itemStock': 12
+   },{
+      "itemId": 9,
+      "itemName": "",
+      'itemPrice': 2.00,
+      'itemStock': 3
+   },
+#Unfortunately, the first attempt to full categorize each food group would prevent the code from running and result in INVALID.
+# so to give a more neat presentation, i will use the comments as borderlines and seperate each food group.
+
+{
+      "itemId": 10,
+      "itemName": "Tiffany FINNS CREAMY CHEDDAR",
+      'itemPrice': 5.00,
+      'itemStock': 14
+   },{
+      "itemId": 11,
+      "itemName": "Tiffany FINNS SALT & VINEGAR",
+      'itemPrice': 5.00,
+      'itemStock': 15
+   },{
+      "itemId": 12,
+      "itemName": "Doritos Sweet Chili Pepper",
+      'itemPrice': 8.00,
+      'itemStock': 15
+   },{
+      "itemId": 13,
+      "itemName": "Doritos Nacho Cheese",
+      'itemPrice': 8.00,
+      'itemStock': 15
+   },{
+      "itemId": 14,
+      "itemName": "POPCORNERS WHITE CHEDDAR flavor",
+      'itemPrice': 12.00,
+      'itemStock': 15
+   },{
+      "itemId": 15,
+      "itemName": "POPCORNERS CINNAMON CRUNCH flavor",
+      'itemPrice': 12.00,
+      'itemStock': 15
+   },{
+      "itemId": 16,
+      "itemName": "POPCORNERS SEA SALT flavor",
+      'itemPrice': 12.00,
+      'itemStock': 15
+   },{
+      "itemId": 17,
+      "itemName": "Ruffles Original Flavor",
+      'itemPrice': 8.00,
+      'itemStock': 15
+   },{
+      "itemId": 18,
+      "itemName": "Ruffles Jalapeño Ranch Flavor",
+      'itemPrice': 8.00,
+      'itemStock': 15
+   },{
+      "itemId": 19,
+      "itemName": "Ruffles Flamin' Hot Flavor",
+      'itemPrice': 8.00,
+      'itemStock': 15
+   },
+#The food group above is categorize as Chips, and the items below are best served with the help of an employee inside the store.   
+{
+      "itemId": 20,
+      "itemName": "Chicken Leg + Thigh BBQ",
+      'itemPrice': 14.00,
+      'itemStock': 12
+   },{
+      "itemId": 21,
+      "itemName": "Chicken skewer BBQ",
+      'itemPrice': 14.00,
+      'itemStock': 15
+   },{
+      "itemId": 22,
+      "itemName": "Chicken Curry Rice Bowl",
+      'itemPrice': 15.00,
+      'itemStock': 15
+   },{
+      "itemId": 23,
+      "itemName": "Egg fried rice & Chicken Thigh Bowl",
+      'itemPrice': 19.00,
+      'itemStock': 9
+   },{
+      "itemId": 24,
+      "itemName": "Chicken Sisig Rice Bowl",
+      'itemPrice': 22.00,
+      'itemStock': 12
+   },{
+      "itemId": 25,
+      "itemName": "Hotdogsilog Breakfast bowl",
+      'itemPrice': 22.00,
+      'itemStock': 9
+   },{
+      "itemId": 26,
+      "itemName": "Sweet Longsilog Breakfast bowl",
+      'itemPrice': 22.00,
+      'itemStock': 10
+   },{
+      "itemId": 27,
+      "itemName": "Creamy Chicken noodle soup bowl",
+      'itemPrice': 12.00,
+      'itemStock': 21
+   },{
+      "itemId": 28,
+      "itemName": "Chunky Beef & Veggie noodle soup bowl ",
+      'itemPrice': 12.00,
+      'itemStock': 42
+   },{
+      "itemId": 29,
+      "itemName": "Seafood Savour noodle soup Bowl",
+      'itemPrice': 12.00,
+      'itemStock': 3
+   },
+#For drinks, this will show the customer the varied items fit for drinking needs
+#be it as necessity as Drinking Water to preferred flavored bottled soda and juice drinks.   
+{
+      "itemId": 30,
+      "itemName": "Bottled water 300ml",
+      'itemPrice': 1.00,
+      'itemStock': 14
+   },{
+      "itemId": 31,
+      "itemName": "Pepsi can 250ml",
+      'itemPrice': 3.00,
+      'itemStock': 15
+   },{
+      "itemId": 32,
+      "itemName": "Coca-Cola can 250ml",
+      'itemPrice': 3.00,
+      'itemStock': 6 
+   },{
+      "itemId": 33,
+      "itemName": "Lipton Iced Tea 450ml",
+      'itemPrice': 5.00,
+      'itemStock': 7
+   },{
+      "itemId": 34,
+      "itemName": "Bottled Pepsi 500ml",
+      'itemPrice': 4.00,
+      'itemStock': 8
+   },{
+      "itemId": 35,
+      "itemName": "Bottled Coca-Cola 500ml",
+      'itemPrice': 4.00,
+      'itemStock': 10
+   },{
+      "itemId": 36,
+      "itemName": "Lipton Iced Tea 1 Liter",
+      'itemPrice': 14.00,
+      'itemStock': 15
+   },{
+      "itemId": 37,
+      "itemName": "Bottled Water 1.5 Liter",
+      'itemPrice': 11.00,
+      'itemStock': 0
+   },{
+      "itemId": 38,
+      "itemName": "Bottled Pepsi 1.5 Liter",
+      'itemPrice': 16.00,
+      'itemStock': 12
+   },{
+      "itemId": 37,
+      "itemName": "Bottled Coca-Cola 1.5 Liter",
+      'itemPrice': 16.00,
+      'itemStock': 6
+   },{
+      "itemId": 38,
+      "itemName": "Bottled Pepsi 3 Liter",
+      'itemPrice': 22.00,
+      'itemStock': 14
+   },{
+      "itemId": 39,
+      "itemName": "Bottled Coca-Cola 3 Liter",
+      'itemPrice': 22.00,
+      'itemStock': 15
+
+}]
+#After the 39 items are listed, I'd like to add the vending machine to provide tissues as a bonus for messy eaters and the code that is required in order for the vending machine to actually run.
+#This is where the fun begins ;v;
+
+balance=int(input("Enter amount: "))
+item = []
+reciept = """
+\t\tPRODUCT NAME -- COST
+"""
+sum = 0
+run = True
+
+print("""░██████╗███╗░░██╗███████╗░█████╗░██╗░░██╗██╗░░░██╗░██████╗███╗░░██╗░█████╗░░█████╗░██╗░░██╗░██████╗
+██╔════╝████╗░██║██╔════╝██╔══██╗██║░██╔╝╚██╗░██╔╝██╔════╝████╗░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝
+╚█████╗░██╔██╗██║█████╗░░███████║█████═╝░░╚████╔╝░╚█████╗░██╔██╗██║███████║██║░░╚═╝█████═╝░╚█████╗░
+░╚═══██╗██║╚████║██╔══╝░░██╔══██║██╔═██╗░░░╚██╔╝░░░╚═══██╗██║╚████║██╔══██║██║░░██╗██╔═██╗░░╚═══██╗
+██████╔╝██║░╚███║███████╗██║░░██║██║░╚██╗░░░██║░░░██████╔╝██║░╚███║██║░░██║╚█████╔╝██║░╚██╗██████╔╝
+╚═════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░╚═╝░░╚══╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+░█████╗░░█████╗░░░░  ██╗░░░██╗███████╗███╗░░██╗██████╗░██╗███╗░░██╗░██████╗░
+██╔══██╗██╔══██╗░░░  ██║░░░██║██╔════╝████╗░██║██╔══██╗██║████╗░██║██╔════╝░
+██║░░╚═╝██║░░██║░░░  ╚██╗░██╔╝█████╗░░██╔██╗██║██║░░██║██║██╔██╗██║██║░░██╗░
+██║░░██╗██║░░██║░░░  ░╚████╔╝░██╔══╝░░██║╚████║██║░░██║██║██║╚████║██║░░╚██╗
+╚█████╔╝╚█████╔╝██╗  ░░╚██╔╝░░███████╗██║░╚███║██████╔╝██║██║░╚███║╚██████╔╝
+░╚════╝░░╚════╝░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░
+
+███╗░░░███╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗███████╗██╗██╗██╗
+████╗░████║██╔══██╗██╔══██╗██║░░██║██║████╗░██║██╔════╝██║╚█║╚█║
+██╔████╔██║███████║██║░░╚═╝███████║██║██╔██╗██║█████╗░░██║░╚╝░╚╝
+██║╚██╔╝██║██╔══██║██║░░██╗██╔══██║██║██║╚████║██╔══╝░░╚═╝░░░░░░
+██║░╚═╝░██║██║░░██║╚█████╔╝██║░░██║██║██║░╚███║███████╗██╗░░░░░░
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░░░░░----\n\n""")
+print("----------The Items currently available are----------\n\n")
+
+#"for" function in Sneaky_snacks so the code will run properly...
+for i in Sneaky_snacks:
+   print(f"Item: {i['itemName']} --- Price: {i['itemPrice']} --- Item ID: {i['itemId']} --- Stock: {i['itemStock']}")
+
+#"def" function and while loop in vendingMachine to help the code to run in the correct way.
+def vendingMachine(balance, items_data, run, item):
+   while run:
+      """for i in Sneaky_snacks:
+         if balance>=i["itemPrice"]:
+            balance-=i["itemPrice"]"""
+
+         
+      buyItem = int(
+         input("\n\nEnter the item code for the item you want to buy: "))
+      if buyItem < len(items_data):
+         item.append(items_data[buyItem])
+      else:
+         print("THE PRODUCT ID IS INCORRECT!")
+      moreItems = str(
+         input("type any key to add more products, and type X to stop:  "))
+      if moreItems.upper() == "X":
+         createReceipt
+      receiptValue = int(
+        input(("1. Print the receipt? 2. Only print the total sum: ")))
+      if receiptValue == 1:
+         print(createReceipt(item, reciept))
+      elif receiptValue == 2:
+         print(sumItem(item))
+      else:
+         print("INVALID")
+
+
+def sumItem(item):
+   sumItems = 0
+   for i in item:
+      sumItems += i["itemPrice"]
+   return sumItems
+
+
+
+def createReceipt(item, reciept):
+   for i in item:
+      reciept += f"""
+      \t{i["itemName"]} -- {i['itemPrice']}
+      """
+   reciept += f"""
+      \tTotal --- {sumItem(item)}
+      """
+   return reciept
+
+def stock(Sneaky_snacks):
+   for i in Sneaky_snacks:
+      i[("itemStock")]-=1
+print("Thank you for your purchase.")
+
+# Main Code flow...
+vendingMachine(balance, Sneaky_snacks, run, item)
